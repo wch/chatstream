11d85b0b15a5c15b2f30bad89fab5ac3ce032e53
Winston Chang <winston@stdout.org>
2023-03-21 08:20:43 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from typing import AsyncGenerator, Literal, TypedDict
+
+import keys
+import openai
+
+from shiny import reactive
+
+openai.api_key = keys.openai_api_key
+
+
+class Usage(TypedDict):
+    completion_tokens: int
+    prompt_tokens: int
+    total_tokens: int
+
+
+class Message(TypedDict):
+    content: str
+    role: str
+
+
+class Choices(TypedDict):
+    finish_reason: str
+    index: int
+    message: Message
+
+
+class ChatCompletion(TypedDict):
+    id: str
+    object: Literal["chat.completion"]
+    created: int
+    model: str
+    usage: Usage
+    choices: list[Choices]
+
+
+async def do_query_streaming(
+    message: str, v: reactive.Value[str]
+) -> AsyncGenerator[str, None]:
+    for resp in openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ],
+        stream=True,
+    ):
+        if "content" in resp.choices[0].delta:
+            yield resp.choices[0].delta.content
+        else:
+            yield ""
+
+
+def do_query(message: str) -> str:
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ],
+    )
+
+    res: ChatCompletion = response.to_dict()
+    res["usage"] = res["usage"].to_dict()
+    res["choices"] = [choice.to_dict() for choice in res["choices"]]
+
+    print(res)
+    return res["choices"][0]["message"]["content"]
---
+++ app.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import asyncio
+from typing import AsyncGenerator
+
+import api
+
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
+
+app_ui = ui.page_fluid(
+    ui.input_text_area(
+        "query",
+        "Dear GPT-3,",
+        value="Tell me about yourself.",
+        placeholder="Ask me anything...",
+        width="100%",
+    ),
+    ui.input_action_button("ask", "Ask"),
+    ui.div(
+        {
+            "style": "border: 1px solid black; border-radius: 4px; padding: 5px; margin-top: 10px;"
+        },
+        ui.output_ui("response"),
+    ),
+)
+
+
+async def set_val_streaming(
+    v: reactive.Value[str], stream: AsyncGenerator[str, None], session: Session
+):
+    async for tok in stream:
+        v.set(v.get() + tok)
+        await reactive.flush()
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    chat_string: reactive.Value[str] = reactive.Value("")
+
+    @reactive.Effect
+    @reactive.event(input.ask)
+    def _():
+        chat_string.set("")
+        asyncio.Task(
+            set_val_streaming(
+                chat_string, api.do_query_streaming(input.query(), chat_string), session
+            )
+        )
+
+        # chat_string.set(api.do_query(input.query()))
+
+    @output
+    @render.ui
+    def response():
+        return ui.markdown(chat_string())
+
+
+app = App(app_ui, server)
