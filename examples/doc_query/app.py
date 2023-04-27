9845861a193ec8f50b1dbb57e3a81580704ee616
Winston Chang <winston@stdout.org>
2023-04-27 10:00:29 -0700

    Add token limits to doc_query app

--- examples/doc_query/app.py
+++ examples/doc_query/app.py
@@ -10,6 +10,7 @@ import chromadb  # pyright: ignore[reportMissingTypeStubs]
 import chromadb.api  # pyright: ignore[reportMissingTypeStubs]
 import pypdf
 import shiny.experimental as x
+import tiktoken
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 from shiny.types import FileInfo
@@ -17,8 +18,13 @@ from shiny.types import FileInfo
 import chat_ai
 from chat_ai import openai_api
 
+MODEL = "gpt-3.5-turbo"
+# Maximum number of context chunks to send to the API.
+N_DOCUMENTS = 16
+# Maximum number of tokens in the context chunks to send to the API.
+CONTEXT_TOKEN_LIMIT = 3200
 # Print debugging info to the console
-debug = True
+DEBUG = True
 
 # Avoid the following warning:
 # huggingface/tokenizers: The current process just got forked, after parallelism has
@@ -44,14 +50,6 @@ app_ui = x.ui.page_fillable(
             ui.input_file("file", "Upload text or PDF files", multiple=True),
             ui.hr(),
             ui.output_ui("uploaded_filenames_ui"),
-            ui.hr(),
-            ui.input_slider(
-                "n_documents",
-                "Number of context chunks to send",
-                min=2,
-                max=12,
-                value=8,
-            ),
             ui.hr(),
             {"class": "sticky-sm-top", "style": "top: 15px;"},
             ui.p(ui.h5("Export conversation")),
@@ -98,12 +96,21 @@ def server(input: Inputs, output: Outputs, session: Session):
     def add_context_to_query(query: str) -> str:
         results = collection.query(
             query_texts=[query],
-            n_results=min(collection.count(), input.n_documents()),
+            n_results=min(collection.count(), N_DOCUMENTS),
         )
+
         if results["documents"] is None:
             context = "No context found"
         else:
-            context = "\n\n".join(results["documents"][0])
+            token_count = 0
+            context = ""
+            for doc in results["documents"][0]:
+                result_token_count = get_token_count(doc, MODEL)
+                if token_count + result_token_count >= CONTEXT_TOKEN_LIMIT:
+                    break
+
+                token_count += result_token_count
+                context += doc + "\n\n"
 
         prompt_template = f"""Use these pieces of context to answer the question at the end.
         You can also integrate other information that you know.
@@ -116,7 +123,7 @@ def server(input: Inputs, output: Outputs, session: Session):
         Answer:
         """
 
-        if debug:
+        if DEBUG:
             print(json.dumps(results, indent=2))
             print(prompt_template)
 
@@ -136,7 +143,7 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         for file in file_infos:
             add_file_content_to_db(
-                collection, file["datapath"], file["name"], debug=debug
+                collection, file["datapath"], file["name"], debug=DEBUG
             )
             with reactive.isolate():
                 uploaded_filenames.set(uploaded_filenames() + (file["name"],))
@@ -243,3 +250,8 @@ def add_file_content_to_db(
         ],
         ids=[f"{label}-{i}" for i in range(len(text_chunks))],
     )
+
+
+def get_token_count(s: str, model: str) -> int:
+    encoding = tiktoken.encoding_for_model(model)
+    return len(encoding.encode(s))
