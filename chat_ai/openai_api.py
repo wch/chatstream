ff1a6b8ed963191a99df7689c8a79e0180248a65
Winston Chang <winston@stdout.org>
2023-04-27 10:18:25 -0700

    Remove unused code

--- chat_ai/openai_api.py
+++ chat_ai/openai_api.py
@@ -1,35 +1,6 @@
 from __future__ import annotations
 
-import os
 from typing import Literal, TypedDict
-
-import openai
-
-# Try to load the key from env var first, and then from file keys.py.
-missing_key_error_text = (
-    "No OpenAI API key found. "
-    "It must be either set in the environment variable OPENAI_API_KEY, or in a "
-    "file named `keys.py`, with a variable named `openai_api_key`."
-)
-
-api_key: str | None = os.environ.get("OPENAI_API_KEY")
-
-if api_key is None:
-    from pathlib import Path
-
-    keys_file = Path(__file__).parent / "keys.py"
-    if not keys_file.exists():
-        raise RuntimeError(missing_key_error_text)
-
-    import keys
-
-    if not hasattr(keys, "openai_api_key"):
-        raise RuntimeError(missing_key_error_text)
-
-    api_key = keys.openai_api_key
-
-openai.api_key = api_key
-
 
 OpenAiModels = Literal[
     "gpt-3.5-turbo",
