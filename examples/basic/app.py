c3933ddb568b6793d2aa80bd27b0e8d8c8babbab
Winston Chang <winston@stdout.org>
2023-05-04 15:41:19 -0500

    Change module id in example

--- examples/basic/app.py
+++ examples/basic/app.py
@@ -3,12 +3,12 @@ from shiny import App, Inputs, Outputs, Session, ui
 import chat_ai
 
 app_ui = ui.page_fixed(
-    chat_ai.chat_ui("chat1"),
+    chat_ai.chat_ui("mychat"),
 )
 
 
 def server(input: Inputs, output: Outputs, session: Session):
-    chat_ai.chat_server("chat1")
+    chat_ai.chat_server("mychat")
 
 
 app = App(app_ui, server)
