60fb3985258c3fccbd77e11f2034510722f9c8e3
Winston Chang <winston@stdout.org>
2023-09-15 18:47:08 -0500

    Update dual chat app

--- examples/dual/app.py
+++ examples/dual/app.py
@@ -21,7 +21,12 @@ app_ui = x.ui.page_fillable(
         x.ui.sidebar(
             ui.h4("Shiny ChatGPT"),
             ui.hr(),
-            ui.p("Model: gpt-3.5-turbo"),
+            ui.input_select(
+                "model",
+                "Model",
+                choices=["gpt-3.5-turbo", "gpt-4"],
+                selected="gpt-3.5-turbo",
+            ),
             ui.input_slider(
                 "temperature",
                 ui.span(
@@ -37,13 +42,13 @@ app_ui = x.ui.page_fillable(
                 value=0.7,
                 step=0.05,
             ),
-            ui.input_switch("auto_converse", "Converse with self"),
+            ui.input_switch("auto_converse", "Auto-conversation", value=True),
             ui.input_slider(
                 "auto_converse_delay",
                 "Conversation delay (seconds)",
                 min=0,
                 max=3,
-                value=1,
+                value=2.4,
                 step=0.2,
             ),
             ui.hr(),
@@ -79,18 +84,30 @@ app_ui = x.ui.page_fillable(
 
 
 def server(input: Inputs, output: Outputs, session: Session):
-    chat_session1 = chatstream.chat_server("chat1", temperature=input.temperature)
-    chat_session2 = chatstream.chat_server("chat2", temperature=input.temperature)
+    chat_session1 = chatstream.chat_server(
+        "chat1",
+        model=input.model,
+        temperature=input.temperature,
+    )
+    chat_session2 = chatstream.chat_server(
+        "chat2",
+        model=input.model,
+        temperature=input.temperature,
+    )
 
     # Which chat module has the most recent completed response from the server.
-    most_recent = reactive.Value(0)
+    most_recent_module = reactive.Value(0)
 
     @reactive.Effect
     @reactive.event(chat_session1.session_messages)
     def _():
         with reactive.isolate():
-            if not input.auto_converse() or most_recent() == 1:
+            if not input.auto_converse() or most_recent_module() == 1:
                 return
+
+        # Don't try to converse if there are no messages.
+        if len(chat_session1.session_messages()) == 0:
+            return
 
         last_message = chat_session1.session_messages()[-1]
         if last_message["role"] == "assistant":
@@ -98,14 +115,18 @@ def server(input: Inputs, output: Outputs, session: Session):
                 last_message["content"],
                 input.auto_converse_delay(),
             )
-            most_recent.set(1)
+            most_recent_module.set(1)
 
     @reactive.Effect
     @reactive.event(chat_session2.session_messages)
     def _():
         with reactive.isolate():
-            if not input.auto_converse() or most_recent() == 2:
+            if not input.auto_converse() or most_recent_module() == 2:
                 return
+
+        # Don't try to converse if there are no messages.
+        if len(chat_session2.session_messages()) == 0:
+            return
 
         last_message = chat_session2.session_messages()[-1]
         if last_message["role"] == "assistant":
@@ -113,7 +134,7 @@ def server(input: Inputs, output: Outputs, session: Session):
                 last_message["content"],
                 input.auto_converse_delay(),
             )
-            most_recent.set(2)
+            most_recent_module.set(2)
 
 
 app = App(app_ui, server)
